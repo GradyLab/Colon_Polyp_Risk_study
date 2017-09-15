70feedb4678f70ac613c43e6e3a38a5f5da1f0b8
smaden <metamaden@users.noreply.github.com>
2017-09-15 12:49:25 -0700

    Create dvmc-validated_table-summary.R

---
+++ primary/dvmc-validated_table-summary.R
@@ -0,0 +1,37 @@
+#==============================
+# validated dvmc summary stats
+#==============================
+# Author: Sean Maden
+
+x <- roc.epic[roc.epic$auc>=0.7,]
+x <- x[rev(order(x$auc)),]; head(x)
+
+ge <- gfilt.epic[rownames(gfilt.epic) %in% x$cpgid,];
+ge <- ge[order(match(rownames(ge),x$cpgid)),]; identical(rownames(ge),x$cpgid)
+
+gt <- gtcgatest[rownames(gtcgatest) %in% x$cpgid,]
+gt <- gt[order(match(rownames(gt),x$cpgid)),]; identical(rownames(gt),x$cpgid)
+
+gd <- gfilt.ievora[rownames(gfilt.ievora) %in% x$cpgid,]
+gd <- gd[order(match(rownames(gd),x$cpgid)),]; identical(rownames(gd),x$cpgid)
+
+ad <- as.data.frame(getAnnotation(gd))
+
+summarydf <- data.frame(dvmc.id=x$cpgid,
+                        auc.epic=x$auc,
+                        memean.epic.lr=rowMeans(getBeta(ge[,ge$Risk_Group.index %in% c("0","1")])),
+                        memean.epic.hr=rowMeans(getBeta(ge[,ge$Risk_Group.index %in% c("2A","2B","3")])),
+                        memean.tcga.t=rowMeans(getBeta(gt[,gt$sampletype=="01"])),
+                        memean.tcga.nm=rowMeans(getBeta(gt[,gt$sampletype=="11"])),
+                        memean.hm450.lr=rowMeans(getBeta(gd[,gd$Risk_Group.index %in% c("0","1")])),
+                        memean.hm450.hr=rowMeans(getBeta(gd[,gd$Risk_Group.index %in% c("2","3")])),
+                        cgisland.id=ad$Islands_Name,
+                        refgene.group=ad$UCSC_RefGene_Group,
+                        refgene.name=ad$UCSC_RefGene_Name,
+                        chr=ad$chr,
+                        pos=ad$pos,
+                        strand=ad$strand)
+
+write.csv(summarydf,file="dvmc-validated_cgsummaries.csv")
+
+###
