0a756ae1160ff30f6f108754e207586c2664453c
smaden <metamaden@users.noreply.github.com>
2017-09-15 12:59:15 -0700

    Update dvmc-validated_table-summary.R

--- primary/dvmc-validated_table-summary.R
+++ primary/dvmc-validated_table-summary.R
@@ -1,7 +1,6 @@
 #==============================
 # validated dvmc summary stats
 #==============================
-# Author: Sean Maden
 
 x <- roc.epic[roc.epic$auc>=0.7,]
 x <- x[rev(order(x$auc)),]; head(x)
@@ -26,6 +25,7 @@ summarydf <- data.frame(dvmc.id=x$cpgid,
                         memean.hm450.lr=rowMeans(getBeta(gd[,gd$Risk_Group.index %in% c("0","1")])),
                         memean.hm450.hr=rowMeans(getBeta(gd[,gd$Risk_Group.index %in% c("2","3")])),
                         cgisland.id=ad$Islands_Name,
+                        cgisland.relation=ad$Relation_to_Island,
                         refgene.group=ad$UCSC_RefGene_Group,
                         refgene.name=ad$UCSC_RefGene_Name,
                         chr=ad$chr,
