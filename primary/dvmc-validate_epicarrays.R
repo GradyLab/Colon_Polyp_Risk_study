6668fcd1218d9e3c6ef2c6aa27bd59bd473014a9
smaden <metamaden@users.noreply.github.com>
2017-09-15 13:09:25 -0700

    Update and rename EPIC_validation.R to dvmc-validate_epicarrays.R

--- primary/dvmc-validate_epicarrays.R
+++ primary/dvmc-validate_epicarrays.R
@@ -18,7 +18,14 @@ table(valvar,gval$Risk_Group.index)
 # 0  7  9  0  0  0
 # 1  0  0  1  1 27
 
+# prefilter on methylation differences (LR<HR)
+bepic <- getBeta(gval); 
+blr <- rowMeans(bepic[,which(valvar==0)]); bhr <- rowMeans(bepic[,which(valvar==1)])
+bepic <- bepic[which(blr<bhr),]; dim(bepic)
+
 # ROC curve/ AUC evaluation
+gval <- gval[rownames(gval) %in% rownames(bepic),]
+
 library(pROC)
 
 roc.epic <- data.frame(cpgid=rownames(gval),
@@ -32,5 +39,7 @@ for(i in 1:nrow(roc.epic)){
 }; roc.epic <- roc.epic[rev(order(roc.epic$auc)),]
 
 valdvmc <- roc.epic[roc.epic$auc>=0.7,]$cpgid # N = 122 CpGs
+
+valdvmc
 
 ###
